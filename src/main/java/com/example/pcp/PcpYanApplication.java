b5fbef867960de75d3da14e3c43aca5ea42db1d2
Yan-Gurgel <yan.gurgel@aluno.uece.br>
2019-09-22 16:41:03 -0300

    Projeto add

---
+++ src/main/java/com/example/pcp/PcpYanApplication.java
@@ -0,0 +1,14 @@
+package com.example.pcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcpYanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcpYanApplication.class, args);
+		System.out.println("TESTE");
+	}
+
+}
