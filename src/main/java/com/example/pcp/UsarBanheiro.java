42069bbd42c1af0b13d8aff9492fa44b363c8812
Yan-Gurgel <yan.gurgel@aluno.uece.br>
2019-10-03 19:55:35 -0300

    Correcao de parametro no metodo banheiro

--- src/main/java/com/example/pcp/UsarBanheiro.java
+++ src/main/java/com/example/pcp/UsarBanheiro.java
@@ -4,10 +4,22 @@ import java.util.ArrayList;
 
 public class UsarBanheiro {
 
-    public void banheiro(ArrayList GeneroA, ArrayList GeneroB, ArrayList GeneroC, Pessoa[] banheiro){
+    public void banheiro(ArrayList<Pessoa> filaGeneroA, ArrayList<Pessoa> filaGeneroB, ArrayList<Pessoa> filaGeneroC, Pessoa[] banheiro){
+
+        for (int i = 0; i < filaGeneroA.size(); i++ ) {
+            System.out.println("Pessoa : " + filaGeneroA.get(i).getGenero() + ", ID: " + filaGeneroA.get(i).getID());
+        }
+
+        for (int i = 0; i < filaGeneroB.size(); i++ ) {
+            System.out.println("Pessoa : " + filaGeneroB.get(i).getGenero() + ", ID: " + filaGeneroB.get(i).getID());
+        }
+
+        for (int i = 0; i < filaGeneroC.size(); i++ ) {
+            System.out.println("Pessoa : " + filaGeneroC.get(i).getGenero() + ", ID: " + filaGeneroC.get(i).getID());
+        }
 
 
-        
+
 
         System.out.println("Arrays manipulados");
     }
